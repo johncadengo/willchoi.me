edc88d501ccdf8dc23cb7a8c86cd0f9472ce8da6
johncadengo <johncadengo@gmail.com>
2014-06-29 22:31:39 -0400

    Add HTML5 Boilerplate.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route("/")
-def hello():
-    return "Hi there"
+def home():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
